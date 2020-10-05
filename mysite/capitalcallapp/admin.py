4390aa1e872fda96f8545cf2be1601f8f5e86550
Melissa-Cutler <Melissa.Cutler@gmail.com>
2020-10-05 20:20:49 +0100

    working_on_views

--- mysite/capitalcallapp/admin.py
+++ mysite/capitalcallapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Fund, Commitment, Investment, Call
 
 # Register your models here.
+
+admin.site.register(Fund)
+admin.site.register(Commitment)
+admin.site.register(Investment)
+admin.site.register(Call)
+
+
--- mysite/capitalcallapp/views.py
+++ mysite/capitalcallapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http      import HttpResponse
+from django.template  import loader
+from .models          import Fund
+
 
 # Create your views here.
 
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the capitalcallapp index.")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the capitalcallapp index.")
+    L_Funds = Fund.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'L_Funds': L_Funds,
+    }
+    return HttpResponse(template.render(context, request))
+
+
