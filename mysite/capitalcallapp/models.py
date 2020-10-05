d81455f0701ffca28d09c4ca1df803bff9d05dd7
Melissa-Cutler <Melissa.Cutler@gmail.com>
2020-10-05 13:12:09 +0100

    working_on_models

--- mysite/capitalcallapp/models.py
+++ mysite/capitalcallapp/models.py
@@ -8,10 +8,10 @@ from django.utils import timezone
 class Fund(models.Model):
     # I think that Django takes care of creating the id field under the hood
     # fund_id: models.IntegerField(default=0)
-    fund_number: models.Field = models.IntegerField(default=0)
+    fund_number    : models.Field = models.IntegerField(unique=True)
     current_balance: models.Field = models.FloatField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.fund_number) + ", balance $" + str(self.current_balance)
 
 
@@ -19,19 +19,40 @@ class Fund(models.Model):
 class Commitment(models.Model):
     # commitment_id: models.Field = models.IntegerField(default=0)
     # fund_id      :   models.ForeignKey(Fund, on_delete=models.CASCADE)
-    fund: models.ForeignObject = models.ForeignKey(Fund, on_delete=models.CASCADE)
-    date: models.Field = models.DateTimeField('date of commitment')
-    amount_usd: models.Field = models.FloatField(default=0.0)
+    commitment_number: models.Field                  = models.IntegerField(unique=True)
+    fund      : models.ForeignObject                 = models.ForeignKey(Fund, on_delete=models.CASCADE)
+    date      : models.query_utils.DeferredAttribute = models.DateTimeField('date of commitment')
+    amount_usd: models.Field                         = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return "$" + str(self.amount_usd) + " to fund" + str(self.fund.fund_number) + " on " + str(self.date)
 
 
 # a class to describe investments from funds
 class Investment(models.Model):
-    date: models.Field = models.DateTimeField('date of investment')
-    amount_usd: models.Field = models.FloatField(default=0.0)
+    investment_number: models.Field                         = models.IntegerField(unique=True)
+    date             : models.query_utils.DeferredAttribute = models.DateTimeField('date of investment')
+    amount_usd       : models.Field                         = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return "$" + str(self.amount_usd) + " on " + str(self.date)
 
 
 # a call describes an amount of money being called from a fund/commitment and used towards an investment
 class Call(models.Model):
-    amount_usd: models.Field = models.FloatField(default=0)
-    fund_id: models.ForeignObject = models.ForeignKey(Commitment, on_delete=models.CASCADE)
-    investment_id: models.ForeignObject = models.ForeignKey(Investment, on_delete=models.CASCADE)
+    commitment_number                                   = models.IntegerField()
+    investment_number                                   = models.IntegerField()
+    amount_usd   : models.Field                         = models.FloatField(default=0)
+    fund         : models.ForeignObject                 = models.ForeignKey(Fund,       on_delete=models.CASCADE)
+    investment   : models.ForeignObject                 = models.ForeignKey(Investment, on_delete=models.CASCADE)
+    date         : models.query_utils.DeferredAttribute = models.DateTimeField('date of call')
+
+    def __str__(self) -> str:
+        #return "$" + str(self.amount_usd) + " from fund" + str(self.fund.fund_number) + " to investment" + str(self.investment_number) + " on " + str(self.date)
+        return " ".join([
+            "$" + str(self.amount_usd), "from", "fund" + str(self.fund.fund_number),
+            "to", "investment" + str(self.investment_number), "on", str(self.date)
+        ])
+
+
+
