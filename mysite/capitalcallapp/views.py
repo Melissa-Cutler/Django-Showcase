2afc0df0ed9868a534748ac37545d3a3554400c4
Melissa-Cutler <Melissa.Cutler@gmail.com>
2020-10-06 18:16:42 +0100

    Funds Summary pannel is tidier.

--- mysite/capitalcallapp/views.py
+++ mysite/capitalcallapp/views.py
@@ -15,9 +15,11 @@ def index(request):
     Query_Set_All_Funds = Fund.objects.order_by('fund_number')[:]
     L_Funds: List[Dict] = [ fund.getDictionaryRepresentation() for fund in Query_Set_All_Funds ]
     dv = 0
+    f_total_available_usd = sum([ D_Fund["f_current_balance_usd"] for D_Fund in L_Funds ])
     template = loader.get_template('capitalcallapp/home.html')
     context = {
-        'L_Funds': L_Funds,
+        'L_Funds'              : L_Funds,
+        "f_total_available_usd": f_total_available_usd
     }
     return HttpResponse(template.render(context, request))
 
